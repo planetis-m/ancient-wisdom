5632ddb9345f119815597c33fc8bed30685eb33c
Antonis Geralis <b3lievem@yandex.com>
2023-04-21 13:52:10 +0300

    fix

--- src/app.nim
+++ src/app.nim
@@ -23,7 +23,7 @@ var
 
 proc createDom(): VNode =
   result = buildHtml:
-    tdiv(class = "flex flex-col items-center justify-center min-h-screen p-10 bg-blurred bg-center"):
+    tdiv(class = "flex flex-col items-center justify-center min-h-screen p-10 bg-blurred bg-center bg-cover"):
       h1(class = "font-light text-2xl md:text-4xl text-gray-600 mb-8 md:mb-12"):
         text ^"Ancient Wisdom"
       tdiv(class = "relative w-full md:w-3/4 lg:w-1/2"):
